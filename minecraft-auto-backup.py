de2f3a31e4265fef065d118cef1a60e9789c2ff4
Soviaat <94050783+Soviaat@users.noreply.github.com>
2024-06-22 01:18:49 +0200

    Made locale to english

--- minecraft-auto-backup.py
+++ minecraft-auto-backup.py
@@ -3,20 +3,18 @@ import os
 import shutil
 from datetime import datetime
 
-MIN_BACKUPS = 3            # Smallest backup limit
-DEFAULT_MAX_BACKUPS = 5    # Default backup limit
-MAX_BACKUPS_LIMIT = 30     # Largest backup limit
+MIN_BACKUPS = 3        
+DEFAULT_MAX_BACKUPS = 5  
+MAX_BACKUPS_LIMIT = 30   
 
 minecraft_world_path = ""
 backup_parent_path = ""
 max_backups = DEFAULT_MAX_BACKUPS
 backup_trigger_event = "stop_recording"
 
-# Timestamp for backup file name
 def get_timestamp():
     return datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-# Sorting backup files by timestamp
 def list_backup_files(backup_path):
     backup_files = []
     for root, dirs, files in os.walk(backup_path):
@@ -26,14 +24,13 @@ def list_backup_files(backup_path):
     backup_files.sort()
     return backup_files
 
-# Backup function
 def backup_minecraft_world(minecraft_world_path, backup_parent_path, max_backups):
     if not minecraft_world_path:
-        obs.script_log(obs.LOG_WARNING, "A Minecraft világ mappáját nem adtad meg.")
+        obs.script_log(obs.LOG_WARNING,"You haven't specified the path to your Minecraft world.")
         return
 
     if not backup_parent_path:
-        obs.script_log(obs.LOG_WARNING, "A backup mappát nem adtad meg.")
+        obs.script_log(obs.LOG_WARNING, "You haven't specified a backup folder.")
         return
 
     world_folder_name = os.path.basename(minecraft_world_path)
@@ -46,16 +43,15 @@ def backup_minecraft_world(minecraft_world_path, backup_parent_path, max_backups
 
     if len(files) >= max_backups:
         oldest_file = files.pop(0)
-        obs.script_log(obs.LOG_INFO, f"Legrégebbi backup törölve: {oldest_file}")
+        obs.script_log(obs.LOG_INFO, f"Oldest backup has been deleted: {oldest_file}")
         os.remove(oldest_file)
 
     timestamp = get_timestamp()
     backup_filename = os.path.join(world_backup_path, f"{world_folder_name}_backup_{timestamp}.zip")
 
     shutil.make_archive(os.path.splitext(backup_filename)[0], 'zip', minecraft_world_path)
-    obs.script_log(obs.LOG_INFO, f"{world_folder_name} backup sikeresen elkészült: {backup_filename}")
+    obs.script_log(obs.LOG_INFO, f"{world_folder_name} backup has been successfully created: {backup_filename}")
 
-# Callback when stream is stopped
 def on_event(event):
     global backup_parent_path
     global max_backups
@@ -66,21 +62,20 @@ def on_event(event):
     elif event == obs.OBS_FRONTEND_EVENT_RECORDING_STOPPED and backup_trigger_event == "stop_recording":
         backup_minecraft_world(minecraft_world_path, backup_parent_path, max_backups)
 
-# Script init
 def script_description():
-    return f"A 'Stop Streaming' gomb megnyomása után backupot készít a megadott Minecraft világról, állítható számú (minmax 3-30) backup megtartásával."
+    return f"Creates a backup of the specified Minecraft world after clicking the \"Stop Streaming/Recording\" button, keeping a minimum of 3 and a maximum of 30 backups."
 
 def script_properties():
-    props = obs.obs_properties_create()
 
-    obs.obs_properties_add_path(props, "minecraft_world_path", "Világ mappájának útvonala:", obs.OBS_PATH_DIRECTORY, "", None)
-    obs.obs_properties_add_path(props, "backup_parent_path", "Backup mappa útvonala:", obs.OBS_PATH_DIRECTORY, "", None)
+    props = obs.obs_properties_create()
+    obs.obs_properties_add_path(props, "minecraft_world_path", "Path to world folder:", obs.OBS_PATH_DIRECTORY, "", None)
+    obs.obs_properties_add_path(props, "backup_parent_path", "Path to backup folder:", obs.OBS_PATH_DIRECTORY, "", None)
     obs.obs_properties_add_int(props, "max_backups", "Maximum backupok száma:", MIN_BACKUPS, MAX_BACKUPS_LIMIT, 1)
 
-    backup_event_menu = obs.obs_properties_add_list(props, "backup_trigger_event", "Készítsen backupot ha:", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
-    obs.obs_property_list_add_string(backup_event_menu, "Élő adást leállítom", "stop_streaming")
-    obs.obs_property_list_add_string(backup_event_menu, "Felvételt leállítom", "stop_recording")
-    obs.obs_property_set_long_description(backup_event_menu, "Válaszd ki, hogy mikor készítsen backupot a Minecraft világról.")
+    backup_event_menu = obs.obs_properties_add_list(props, "backup_trigger_event", "Make backup on:", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_STRING)
+    obs.obs_property_list_add_string(backup_event_menu, "Stop Streaming", "stop_streaming")
+    obs.obs_property_list_add_string(backup_event_menu, "Stop Recording", "stop_recording")
+    obs.obs_property_set_long_description(backup_event_menu, "Choose when to make a backup of your world.")
     
     return props
 
